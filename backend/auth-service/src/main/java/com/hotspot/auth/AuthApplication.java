6ffe1fc90f9da190a46bd91213f306232d6a5953
1004joonsuk <1004joonsuk@naver.com>
2023-10-18 17:12:54 +0900

    :zap: feat: 시큐리티, 디스커버리 설정

--- backend/auth-service/src/main/java/com/hotspot/auth/AuthApplication.java
+++ backend/auth-service/src/main/java/com/hotspot/auth/AuthApplication.java
@@ -2,8 +2,11 @@ package com.hotspot.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@EnableEurekaServer
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class AuthApplication {
 
 	public static void main(String[] args) {
