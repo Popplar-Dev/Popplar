09a4925d5065da87d53127dca99d826985acba21
김민석 <kmin9811@naver.com>
2023-10-19 14:01:32 +0900

    :sparkles: feat: Member, Stamp Repository 생성

---
+++ backend/member-service/src/main/kotlin/com/hotspot/member/repository/MemberRepository.kt
@@ -0,0 +1,7 @@
+package com.hotspot.member.repository;
+
+import com.hotspot.member.entity.Member
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface MemberRepository : JpaRepository<Member, Long> {
+}
