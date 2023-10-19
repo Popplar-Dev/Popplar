35d30a9aed59fead602aad60b484bfbd8195654b
김민석 <kmin9811@naver.com>
2023-10-19 16:29:24 +0900

    :wrench: edit: OAuth WebClient 위해 webflux 추가

--- backend/member-service/build.gradle.kts
+++ backend/member-service/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	compileOnly("org.projectlombok:lombok")
