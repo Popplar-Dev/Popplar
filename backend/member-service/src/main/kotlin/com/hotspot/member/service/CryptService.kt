24102347e472ab59c58415ec12d57f077990b23c
김민석 <kmin9811@naver.com>
2023-10-23 15:56:42 +0900

    pencil2: edit: 사용하지 않는 AES 제거

--- backend/member-service/src/main/kotlin/com/hotspot/member/service/CryptService.kt
+++ backend/member-service/src/main/kotlin/com/hotspot/member/service/CryptService.kt
@@ -4,21 +4,10 @@ import com.hotspot.member.dto.MemberProfileResDto
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.nio.ByteBuffer
-import java.util.*
-import javax.crypto.Cipher
-import javax.crypto.SecretKey
-import javax.crypto.spec.SecretKeySpec
-
 
 @Service
 @Transactional(readOnly = true)
 class CryptService (
-
-    @Value("\${ALGORITHM}")
-    private val ALGORITHM: String,
-    @Value("\${SECRET}")
-    private val SECRET: String,
 
     @Value("\${SALT_A}")
     private val saltA: Long,
