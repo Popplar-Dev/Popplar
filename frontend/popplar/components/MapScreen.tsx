0f7b18322897b8f364789b97ef65cc0ae96c244c
paengjiwoo <aprilzu0427@gmail.com>
2023-10-24 16:20:01 +0900

    :sparkles: feat: MapScreen WebView 추가

--- frontend/popplar/components/MapScreen.tsx
+++ frontend/popplar/components/MapScreen.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import WebView from 'react-native-webview';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const MapScreen: React.FC = () => {
   return (
-    <View >
+    <View style={styles.container}>
       <Text>Map</Text>
+      <WebView 
+      style={styles.webview}
+      source={{uri: 'https://naver.com'}}
+      />
     </View>
   );
 };
 
 export default MapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  webview: {
+    flex: 1,
+    width: windowWidth,
+    height: windowHeight,
+  }
+})
