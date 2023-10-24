aa927d1474cbe3e7d43ccbfb51f04bec65fde5a7
paengjiwoo <aprilzu0427@gmail.com>
2023-10-24 13:30:31 +0900

    :sparkles: feat: kakao-map 화면 띄우기

--- frontend/popplar-map/src/App.tsx
+++ frontend/popplar-map/src/App.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 
 import Map from './pages/Map'
 
 function App() {
+
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
     <div className="App">
       <Map />
