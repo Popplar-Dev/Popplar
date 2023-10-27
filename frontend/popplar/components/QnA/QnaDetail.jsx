ef5874aa7e92d5869c08f36b82c4b835f72057bd
seo donghhon <gns9541gns@naver.com>
2023-10-27 13:53:36 +0900

    :sparkles: feat: 업적 모달 수정

--- frontend/popplar/components/QnA/QnaDetail.jsx
+++ frontend/popplar/components/QnA/QnaDetail.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import { View, Text, StyleSheet, ImageBackground, Switch } from 'react-native';
 
 export default function QnaDetail({ route }) {
-  // route.params에서 전달된 질문 데이터를 받아옵니다.
   const { qnaData } = route.params;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{qnaData.username}</Text>
-      <Text style={styles.text}>{qnaData.date}</Text>
-      <Text style={styles.text}>Q. {qnaData.question}</Text>
-      {qnaData.answers.map((ans, index) => (
-      <View style={styles.qnabox} key={index}>
-        <Text style={styles.text}>A{index+1}. {ans}</Text>
+      <View style={styles.qnacontainer}>
+        <View style={styles.questionboxtop}>
+          <Text style={styles.text}>{qnaData.username}</Text>
+          <Text style={styles.text}>{qnaData.date}</Text>
+        </View>
+        <View style={styles.questionboxbottom}>
+          <Text style={styles.text}>Q. {qnaData.question}</Text>
+        </View>
+        <View style={styles.answerbox}>
+          {qnaData.answers.map((ans, index) => (
+          <View style={styles.qnabox} key={index}>
+            <Text style={styles.text}>A{index+1}. {ans}</Text>
+          </View>
+          ))}
+        </View>
       </View>
-      ))}
     </View>
   );
 }
@@ -26,8 +33,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'transparent',
   },
+  qnacontainer: {
+    backgroundColor:'rgba(139, 144, 247, 0.3)',
+    padding:20,
+    width:'90%',
+    height:'80%',
+    borderRadius:20
+  },
   text: {
     color: 'white',
-    fontSize: 15,
+    fontSize: 18,
   },
+  questionboxtop: {
+		flexDirection:'row',
+		justifyContent: 'space-between',
+		// marginBottom:10,
+    marginVertical:10
+	},
+  questionboxbottom: {
+    marginVertical:10
+  },
+  answerbox: {
+    marginVertical:10
+  }
 });
