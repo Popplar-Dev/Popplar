29f460c0b2435a7ccdb196c66a6ebc6ed3db33f8
김민석 <kmin9811@naver.com>
2023-10-30 16:49:22 +0900

    :sparkles: feat: chatting web test 생성

--- backend/live-chat-service/build.gradle.kts
+++ backend/live-chat-service/build.gradle.kts
@@ -23,6 +23,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.webjars:sockjs-client:1.0.2")
+	implementation("org.webjars:webjars-locator-core")
+	implementation("org.webjars:stomp-websocket:2.3.3")
+	implementation("org.webjars:jquery:3.1.1-1")
+	implementation("org.webjars:bootstrap:3.3.7")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
