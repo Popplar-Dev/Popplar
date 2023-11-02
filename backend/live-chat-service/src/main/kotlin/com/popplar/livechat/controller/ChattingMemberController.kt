269f11fbca720aa71bcaf3e9746e1a1fa10ef5d5
OhSeongRak <osr0907@naver.com>
2023-11-02 10:19:10 +0900

    :sparkles: feat: 실시간 채팅 cicd 작성

--- backend/live-chat-service/src/main/kotlin/com/popplar/livechat/controller/ChattingMemberController.kt
+++ backend/live-chat-service/src/main/kotlin/com/popplar/livechat/controller/ChattingMemberController.kt
@@ -2,7 +2,6 @@ package com.popplar.livechat.controller
 
 import com.popplar.livechat.dto.ChattingMemberReqDto
 import com.popplar.livechat.service.ChattingMemberService
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/chatting-member")
+@RequestMapping("/live-chat/chatting-member")
 class ChattingMemberController (
     private val chattingMemberService: ChattingMemberService
 ){
--- backend/member-service/src/main/kotlin/com/hotspot/achievement/controller/AchievementController.kt
+++ backend/member-service/src/main/kotlin/com/hotspot/achievement/controller/AchievementController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/achievement")
+@RequestMapping("member/achievement")
 class AchievementController(
 
     private val achievementService: AchievementService,
