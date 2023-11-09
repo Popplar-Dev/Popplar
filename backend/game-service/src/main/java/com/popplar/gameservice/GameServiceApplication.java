0418dd13e5495fd8210c9818a0035c89339aa2cc
1004joonsuk <1004joonsuk@naver.com>
2023-11-09 14:49:19 +0900

    :zap: feat: kafka 게임 서버와 멤버 서버 세팅 완료 및 각각의 서버가 생산, 소비를 모두 할 수 있도록 함. 카프카 docker로드는 gamesservice의 kafka폴더의 도커 컴포즈 파일로 로드 가능

--- backend/game-service/src/main/java/com/popplar/gameservice/GameServiceApplication.java
+++ backend/game-service/src/main/java/com/popplar/gameservice/GameServiceApplication.java
@@ -2,10 +2,12 @@ package com.popplar.gameservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.popplar.gameservice", "com.popplar.gameservice.config"})
 public class GameServiceApplication {
 
     public static void main(String[] args) {
