d7f6c931ac9b4deeff8d6e62f55fff9fcf953ac3
seo donghhon <gns9541gns@naver.com>
2023-11-15 15:52:32 +0900

    :sparkles: feat: 게임 ui 및 설명 수정

--- frontend/popplar/babel.config.js
+++ frontend/popplar/babel.config.js
@@ -2,20 +2,6 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
-    [
-		'module:react-native-dotenv',
-			{
-				"moduleName": "@env",
-				"path": ".env",
-				"blocklist": null,
-				"allowlist": null,
-				"blacklist": null,
-				"whitelist": null,
-				"safe": false,
-				"allowUndefined": true,
-				"verbose": false
-			}
-	  ]
 		
   ],
 };
--- frontend/popplar/env.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@env" {
-  export const S3_URL: string;
-}
