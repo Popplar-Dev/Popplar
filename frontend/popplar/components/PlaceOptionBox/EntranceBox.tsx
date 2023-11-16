7e2c5f46947792c78ced9db231669fa1397c2394
OhSeongRak <osr0907@naver.com>
2023-11-16 16:21:56 +0900

    :bug: fix: 핫플 등록 안된곳도 입장되는 버그 수정

--- frontend/popplar/components/PlaceOptionBox/EntranceBox.tsx
+++ frontend/popplar/components/PlaceOptionBox/EntranceBox.tsx
@@ -1,45 +1,59 @@
-// EntranceBox.js
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { getAllHotplace, getIdHotplace } from '../services/getHotplace';
 
 type Props = {
     type: 'possible' | 'impossible';
-    spaceId: number;
-    onEnterPress: (spaceId: number) => void
+    spaceId: string;
+    onEnterPress: (spaceId: string) => void
 };
 
 export default function EntranceBox({type, spaceId, onEnterPress}: Props) {
-    const handleEnterPress = () => {
-        onEnterPress(spaceId);
-    };
+  const [isSpaceIdValid, setIsSpaceIdValid] = useState(false);
 
-    return (
-        <View style={styles.container}>
-        {type === 'possible' && (
-          <View style={styles.entrancecontainer}>
+  useEffect(() => {
+    getAllHotplace()
+    .then((res) => {
+      setIsSpaceIdValid(res.data.some(item => item.id === spaceId));
+    })
+  }, [spaceId]);
+
+  const handleEnterPress = () => {
+      onEnterPress(spaceId);
+  };
+
+  return (
+      <View style={styles.container}>
+      {type === 'possible' && (
+        <View style={styles.entrancecontainer}>
+        {isSpaceIdValid ? (
+          <>
             <View style={styles.textbox}>
-              <Text style={styles.buttonText}>핫플레이스와의 거리가 가까워 입장이 가능합니다.</Text>
-              <Text style={styles.buttonText}>입장 시 
-                <Text style={styles.colortext}> 채팅방 </Text>,
-                <Text style={styles.colortext}>게임 </Text>,
-                <Text style={styles.colortext}>업적 추가 </Text>
-                 기능을 사용할 수 있습니다</Text>
-            </View>
-            <TouchableOpacity onPress={handleEnterPress} style={styles.button}>
-                <Text style={styles.buttonText}>입장하기</Text>
-            </TouchableOpacity>
+            <Text style={styles.buttonText}>핫플레이스와의 거리가 가까워 입장이 가능합니다.</Text>
+            <Text style={styles.buttonText}>입장 시 
+              <Text style={styles.colortext}> 채팅방 </Text>,
+              <Text style={styles.colortext}>게임 </Text>,
+              <Text style={styles.colortext}>업적 추가 </Text>
+                기능을 사용할 수 있습니다</Text>
           </View>
-          
-        )}
+          <TouchableOpacity onPress={handleEnterPress} style={styles.button}>
+              <Text style={styles.buttonText}>입장하기</Text>
+          </TouchableOpacity>
+        </>
+          ) : (
+          <>
+            <Text style={styles.buttonText}>아직 등록되지 않은 핫플레이스입니다.</Text>
+          </>
+          )}
+          </View>)}
 
-        {type === 'impossible' && (
-          <View style={styles.entrancecontainer}>
-          <Text style={styles.buttonText}>핫플레이스와 거리가 멀어 입장이 불가능합니다.</Text>
-        </View>
-            
-        )}
-        </View>
+      {type === 'impossible' && (
+        <View style={styles.entrancecontainer}>
+        <Text style={styles.buttonText}>핫플레이스와 거리가 멀어 입장이 불가능합니다.</Text>
+      </View>
+          
+      )}
+      </View>
   );
 };
 
